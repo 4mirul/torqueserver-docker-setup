efbcb2106e8263cdba69971b387149763757efc7
4mirul <muhdamirul.abdaziz@gmail.com>
2021-07-03 12:07:42 +0800

    update

---
+++ cadvisor-docker.bash
@@ -0,0 +1,13 @@
+VERSION=v0.37.5 # use the latest release version from https://github.com/google/cadvisor/releases
+sudo docker run \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:ro \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --volume=/dev/disk/:/dev/disk:ro \
+  --publish=8086:8080 \
+  --detach=true \
+  --name=cadvisor \
+  --privileged \
+  --device=/dev/kmsg \
+  gcr.io/cadvisor/cadvisor:$VERSION
