69a2ba4a23dd2545f9c9680e3fc1bdfbb06f9107
4mirul <muhdamirul.abdaziz@gmail.com>
2021-07-03 10:55:31 +0800

    update

--- prometheus-docker.bash
+++ prometheus-docker.bash
@@ -1,5 +1,6 @@
 # https://prometheus.io/docs/prometheus/latest/installation/
 docker run \
+    --name prometheus \
     -p 9090:9090 \
     -v /opt/prometheus:/etc/prometheus \
     prom/prometheus
