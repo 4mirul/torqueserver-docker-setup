0396c6b30446fb95f5482baac05651f413710030
4mirul <muhdamirul.abdaziz@gmail.com>
2021-07-07 18:04:58 +0800

    update

--- organizr-docker.sh
+++
@@ -1,9 +0,0 @@
-docker create \
-  --name=organizr \
-  -v /opt/organizr/config:/config \
-  -e PGID=1000 \
-  -e PUID=1000  \
-  -p 8089:80 \
-  -e fpm="false" `#optional` \
-  -e branch="v2-master" `#optional` \
-  organizr/organizr
