422e9e49ba10414eef7d3e1b6a20f8acc2f59587
4mirul <muhdamirul.abdaziz@gmail.com>
2021-08-19 12:10:14 +0800

    update

--- influxdb-docker.sh
+++
@@ -1,9 +0,0 @@
-docker run -d -p 8086:8086 \
-      -v /opt/influxdb/data:/var/lib/influxdb2 \
-      -v /opt/influxdb/config:/etc/influxdb2 \
-      -e DOCKER_INFLUXDB_INIT_MODE=setup \
-      -e DOCKER_INFLUXDB_INIT_USERNAME=user \
-      -e DOCKER_INFLUXDB_INIT_PASSWORD=password \
-      -e DOCKER_INFLUXDB_INIT_ORG=org \
-      -e DOCKER_INFLUXDB_INIT_BUCKET=bucket \
-      influxdb:2.0
