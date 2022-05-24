8cba9361b34ec3596440f9b19b3de4aab5bdcf2c
4mirul <amirul@duck.com>
2022-05-24 15:28:02 +0000

    update

--- docker-cli/portainer-install.sh
+++ docker-cli/portainer-install.sh
@@ -1,3 +1,3 @@
 docker volume create portainer_data
 
-docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -l flame.type=app -l flame.name=portainer -l flame.url=http://192.168.0.105:9000/ -l flame.icon=image-multiple portainer/portainer-ce:latest
+docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -l flame.type=app -l flame.name=portainer -l flame.url=http://torqueserver.home:9000/ -l flame.icon=image-multiple portainer/portainer-ce:latest
